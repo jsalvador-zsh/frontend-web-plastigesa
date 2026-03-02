eb867a86fc4e898e71b51ef79253f00965eec757
Juan Salvador <juanstdev@gmail.com>
2026-03-02 15:26:11 +0000

    [ADD] Widget de Chatwoot

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,22 +39,24 @@ export default async function RootLayout({
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-              (function(d,t) {
-                  var BASE_URL="https://rq0yds.easypanel.host";
-                  var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-                  g.src=BASE_URL+"/packs/js/sdk.js";
-                  g.async = true;
-                  s.parentNode.insertBefore(g,s);
-                  g.onload=function(){
-                    window.chatwootSDK.run({
-                      websiteToken: 'av7aQB5Gg1mv3Gr4zgHWa8fR',
-                      baseUrl: BASE_URL
-                    })
-                  }
-                })(document,"script");
+  window.chatwootSettings = {"position":"right","type":"expanded_bubble","launcherTitle":"Conversa con nosotros"};
+  (function(d,t) {
+    var BASE_URL="https://chat.plastigesa.org";
+    var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src=BASE_URL+"/packs/js/sdk.js";
+    g.async = true;
+    s.parentNode.insertBefore(g,s);
+    g.onload=function(){
+      window.chatwootSDK.run({
+        websiteToken: 'tjAMFgBHPhMwoZHXTfXGAKFv',
+        baseUrl: BASE_URL
+      })
+    }
+  })(document,"script");
             `
           }}
         />
+
       </body>
     </html>
   )
